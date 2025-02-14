df9f4825ee96f147acae7bd77192ed89bc5e1e68
tongkiankiat <tong.kiankiat@gmail.com>
2025-02-14 17:25:23 +0800

    Implemented ArrayList method

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -4,9 +4,13 @@ import circus.animal.Animal;
 import circus.animal.Duck;
 import circus.animal.Parrot;
 import circus.animal.Tiger;
+import circus.animal.Elephant;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Circus {
     private static Animal[] animals = {
@@ -40,9 +44,42 @@ public class Circus {
         return total;
     }
 
+    public static void printAnimals(ArrayList<Animal> animalArrayList) {
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
+    }
+
     public static void main(String[] args) {
-        makeAnimalsTalk();
-        System.out.println("Total value of animals " + calculateAssetValue(animals));
-        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+        System.out.println("Number of animals: " + animals.length);
+//        makeAnimalsTalk();
+//        System.out.println("Total value of animals " + calculateAssetValue(animals));
+//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+//         This won't work as the animals is of fixed size
+//        animals[3] = new Elephant("Strong One");
+//        System.out.println("Number of animals: " + animals.length);
+
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+
+        // Substitutability allows us to add specific animals to the "Animal" array list
+        Parrot perry = new Parrot("Perry");
+        animalArrayList.add(perry);
+        animalArrayList.add(new Elephant("Brute"));
+        Duck andy =  new Duck("Andy");
+        animalArrayList.add(andy);
+
+        System.out.println("Size of ArrayList: " + animalArrayList.size());
+
+        System.out.println("Perry is in position: " + animalArrayList.indexOf(perry));
+
+        System.out.println("Before sorting");
+        printAnimals(animalArrayList);
+        System.out.println("Size of ArrayList: " + animalArrayList.size());
+
+        animalArrayList.sort(Animal.AnimalNameComparator);
+
+        System.out.println("After sorting");
+        printAnimals(animalArrayList);
+        System.out.println("Size of ArrayList: " + animalArrayList.size());
     }
 }
